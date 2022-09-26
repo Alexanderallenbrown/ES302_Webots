91068cac36fc32302cb9918c565a539b138da84f
Alexander Brown <brownaa@lafayette.edu>
2022-09-26 10:28:47 -0400

    supervisor fix

--- controllers/A04_Supervisor/A04_Supervisor.py
+++ controllers/A04_Supervisor/A04_Supervisor.py
@@ -1,12 +1,13 @@
 from controller import Supervisor
 robot = Supervisor()  # create Supervisor instance
 #open a file
-f = open("supervisor_data.txt",'w')
+f = open("A04_data.txt",'w')
 #write a format line to the top
-f.write("time, robot_x, robot_y\r\n")
+f.write("time, leader_x, leader_y, follower_x, follower_y\r\n")
 
 # get access to our two robots in the world
-myromi = robot.getFromDef('Romi')
+leader = robot.getFromDef('Romi_Leader')
+follower = robot.getFromDef('Romi_Follower')
 #get the timestep from one of them
 timestep = int(robot.getBasicTimeStep())
 print("supervisor timestep: "+str(timestep))
@@ -18,9 +19,10 @@ simtime = 0
 i = 0
 while robot.step(timestep) != -1:
   #get the positions of each robot
-  myromi_pos = myromi.getPosition()
+  follower_pos = follower.getPosition()
+  leader_pos = leader.getPosition()
   #write them to file in the specified format
-  f.write(str(simtime)+","+str(myromi_pos[0])+","+str(myromi_pos[1])+"\r\n")
+  f.write(str(simtime)+","+str(leader_pos[0])+","+str(leader_pos[1])+","+str(follower_pos[0])+","+str(follower_pos[1])+"\r\n")
   #update simtime
   simtime+=timestep
 
