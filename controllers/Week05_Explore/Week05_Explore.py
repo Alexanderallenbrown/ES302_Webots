69191b09e238a35800f13f5b9f0ba6321ba50e95
Alexander Brown <brownaa@lafayette.edu>
2022-09-27 22:40:39 -0400

    W05 in class

--- controllers/Week05_Explore/Week05_Explore.py
+++ controllers/Week05_Explore/Week05_Explore.py
@@ -9,6 +9,8 @@ from Romi import Romi
 from exploreFSM import exploreFSM,Timer
 import random
 
+#create a file to write results to:
+f = open('Week05_Data.txt','w')
 
 # create the Robot instance.
 #instantiating a Romi library object in simulation mode
@@ -35,9 +37,41 @@ T1 = Timer(1000)
 FSM = exploreFSM()
 proxReading = 0
 
+oldLeftEncoder = 0#value for old left encoder (for velocity computation)
+oldRightEncoder = 0 #old right encoder
+
+#values to hold velocities
+vLeft = 0
+vRight = 0
+
+#variables to hold allocentric romi estimates
+Xromi = 0
+Yromi = 0
+#variables to hold egocentric romi estimates
+U = 0
+psidot = 0
+
+simtime = 0
+
 # Main loop:
 # - perform simulation steps until Webots is stopping the controller
 while romi.simromi.step(timestep) != -1:
+    simtime += timestep/1000.0
+    ### FSM block 1: inputs, timers, and counters
+
+    #compute Romi encoder velocities in COUNTS PER SECOND
+    vLeft = (romi.encLeft - oldLeftEncoder)/(timestep/1000.0)
+    vRight = (romi.encRight - oldRightEncoder)/(timestep/1000.0)
+
+    #use to get estimates of U, psidot using vLeft and vRight
+    U = 0 #  TODO: implement this!
+    psidot = 0 # TODO: implement this!
+
+    #use to get estimates of current romi X, and Y in allocentric frame
+    #using odometry:
+    Xromi = 0 #TODO implement this!
+    Yromi = 0 #TODO implement this!
+
     #update the timers: FSM block 1
     #timer 0 should run when in wait
     T0.update(FSM.WAIT,timestep)
@@ -62,8 +96,5 @@ while romi.simromi.step(timestep) != -1:
         romi.update(0,0,90,90,90)
     proxReading = romi.proxFrontVal
 
-    #first read the GPS sensor
-    gpspos = gpsSensor.getValues()
-
-
-    #print("Prox: "+str(proxReading))
+    #now write Romi's estimates to a file. to start, let's write our X,Y estimates
+    f.write(str(simtime)+","+str(Xromi)+","+str(Yromi)+"\r\n")
