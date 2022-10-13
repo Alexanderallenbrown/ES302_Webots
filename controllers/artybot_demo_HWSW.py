8c8ad59e223de6a3e5ed43b01ca164c1e1ee576c
Alexander Brown <brownaa@lafayette.edu>
2022-10-13 19:25:46 -0400

    artybot update

--- controllers/artybot_demo_HWSW.py
+++ controllers/artybot_demo_HWSW.py
@@ -3,22 +3,17 @@
 # You may need to import some classes of the controller module. Ex:
 #  from controller import Robot, Motor, DistanceSensor
 from controller import Robot, Motor
-from math import sin,cos
+from math import sin
 import sys
 #add ES302 directory to path because Artybot.py is there
 sys.path.append("../ES302_Artybot")
 from Artybot import Artybot
 
 arty = Artybot(sim=True)
+artyHW = Artybot(port='/dev/cu.usbmodem101')#replace with your port!
 
 # get the time step of the current world.
 timestep = int(arty.simartybot.getBasicTimeStep())
-
-#get the position of the pen tip using a 'gps' sensor
-pengps = arty.simartybot.getDevice("gps_penposition")
-pengps.enable(timestep)
-
-datafile = open("penposition.txt",'w')
 
 # sim time for servo positions
 simtime = 0.0
@@ -30,15 +25,11 @@ while arty.simartybot.step(timestep) != -1:
     #compute servo commands to send to Arty
     s1pos = 90
     s2pos = 20*sin(simtime+2)+90
-    s3pos = 20*sin(simtime)+30
+    s3pos = 20*sin(simtime)-60
 
     #update Arty
     arty.update(s1pos,s2pos,s3pos)
+    artyHW.update(s1pos,s2pos,s3pos)
+    pass
 
-    #get pen position
-    penx,peny,penz = pengps.getValues()
-    print(penx,peny,penz)
-    #write values to file
-    datafile.write(str(simtime)+","+str(penx)+","+str(peny)+","+str(penz)+"\r\n")
-
-datafile.close()
+# Enter here exit cleanup code.
