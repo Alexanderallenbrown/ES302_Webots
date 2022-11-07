017c2ae5fc6f35c396a8b870fc30e3ad877f99f5
Alexander Brown <brownaa@lafayette.edu>
2022-11-07 10:25:06 -0500

    Week 11

--- controllers/Week11_Supervisor/Week11_Supervisor.py
+++ controllers/Week11_Supervisor/Week11_Supervisor.py
@@ -34,20 +34,27 @@ im = ImageOps.grayscale(im)
 im2 = im.resize((map_m,map_m))
 #convert image to true black and white
 im2.convert("1")
-im.show("map")
+# im.show("map")
 #load into a numpy array
 map = np.array(im2)
-# print(map)
-
-
 #now loop through and place a crate at every occupied cell
 occupied_thresh = 200 #threshold for grayscale image to consider a pixel occupied
 w,h = map.shape
+#convert map to BINARY occupancy grid
 for i in range(0,h):
     for j in range(0,w):
         if map[i,j]>=occupied_thresh:
+            map[i,j]=1
+        else:
+            map[i,j]=0
+
+
+
+for i in range(0,h):
+    for j in range(0,w):
+        if map[i,j]==1:
             #we have now found an occupied cell. Place a box here.
-            print("found object at i="+str(i)+",j="+str(j))
+            #print("found object at i="+str(i)+",j="+str(j))
             children_field.importMFNodeFromString(-1, "WoodenBox {translation "+str(map_d/2+j*map_d)+" "+str(map_m*map_d - (map_d/2+i*map_d))+" 0.05  size 0.1 0.1 0.1 }")
 
 while robot.step(timestep) != -1:
